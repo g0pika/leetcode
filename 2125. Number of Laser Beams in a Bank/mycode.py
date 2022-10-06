ecda4b437253dc47e17f34b2b331a9409eec7533
Gopika G <71868599+g0pika@users.noreply.github.com>
2022-10-06 18:21:06 +0530

    Create mycode.py

---
+++ 2125. Number of Laser Beams in a Bank/mycode.py
@@ -0,0 +1,23 @@
+from numpy import *
+
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        ls = []
+        sm = 0
+        mul = 0
+        rows = len(bank)
+        cols = len(bank[0])
+        
+        for i in range(0, rows):
+            sumrow = 0
+            for j in range(0, cols):
+                sumrow += int(bank[i][j])
+            if sumrow != 0:
+                ls.append(sumrow)    
+            
+        
+        for i in range(len(ls)-1):
+            mul = ls[i] * ls[i+1]
+            sm += mul
+        return sm
+            
