38bd37c12fe0a1db0f39ddd1e271a739f41152aa
Gopika G <71868599+g0pika@users.noreply.github.com>
2022-10-08 09:19:51 +0530

    Update mergellusingrecursion.py

--- linkedlist/mergellusingrecursion.py
+++ linkedlist/mergellusingrecursion.py
@@ -13,6 +13,6 @@ class Solution:
             list1.next = self.mergeTwoLists(list1.next, list2)
             return list1
         else:
-            list2.next = self.mergeTwoLists(list2.next, list1)
+            list2.next = self.mergeTwoLists(list1, list2.next)
             return list2
                 
